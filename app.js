734fb88e7096bdf12a703f77db30c3481b37aecb
BrianCaetano <brian-caetano@hotmail.com>
2022-05-15 21:48:15 -0300

    feat: special Attack, round count and fix HP bar

--- app.js
+++ app.js
@@ -4,39 +4,54 @@ function randomNumber(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
 Vue.createApp({
   data() {
     return {
       playerHP: 100,
       monsterHP: 100,
+      round: 1,
     };
   },
 
   methods: {
     attack() {
       this.monsterHP -= randomNumber(5, 10);
-      console.log('MonsterHP', this.monsterHP);
       this.receiveDamage();
+      this.round++;
+      console.log("round", this.round);
     },
 
     receiveDamage() {
       this.playerHP -= randomNumber(8, 13);
-      console.log('PlayerHP',this.playerHP);
+    },
+
+    specialAttack() {
+      this.monsterHP -= randomNumber(10, 20);
+      this.receiveDamage();
+      this.round++;
+      console.log("round", this.round);
     },
   },
 
   computed: {
     playerHPstyle() {
+      if (this.playerHP <= 0) {
+        return { width: 0 + "%" };
+      }
+
       return {
-        width: this.playerHP + '%',
+        width: this.playerHP + "%",
       };
     },
 
     monsterHPstyle() {
+      if (this.monsterHP <= 0) {
+        return { width: 0 + "%" };
+      }
+
       return {
-        width: this.monsterHP + '%',
+        width: this.monsterHP + "%",
       };
-    }
-  }
+    },
+  },
 }).mount("#game");
